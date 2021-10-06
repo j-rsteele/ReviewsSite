5be6ecba2fac5c4c903411e19456ba73e50c9a4d
roman <romyli4ka@gmail.com>
2021-10-06 12:03:59 -0400

    classes actor movie review movieactor

---
+++ ReviewsSite/Models/Actor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Models
+{
+	public class Actor
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int MovieId { get; set; }
+		public virtual List<MovieActors> Movies { get; set; }
+	}
+
+}
---
+++ ReviewsSite/Models/MovieActors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Models
+{
+    public class MovieActors
+    {
+        public int Id;
+        public int ActorId { get; set; }
+        public int MovieId { get; set; }
+
+        public virtual Actor Actor { get; set; }
+        public virtual Movie Movie { get; set; }
+
+    }
+}

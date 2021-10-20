ca83ae68e64795ef765b2652b0af93d449d51c26
mosdefinite <d.e.fairchild@gmail.com>
2021-10-20 15:40:32 -0400

    fixed font error and slideshow on home page

---
+++ ReviewsSite/Migrations/20211020184737_UpdatedSeedData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ReviewsSite.Migrations
+{
+    public partial class UpdatedSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
